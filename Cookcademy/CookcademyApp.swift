799cdfa25f1038fd88624e16c0f6a6cbf16f0333
Andre Bezada <a.bezada.maza@gmail.com>
2022-07-27 17:13:28 -0500

    Initial Commit

---
+++ Cookcademy/CookcademyApp.swift
@@ -0,0 +1,17 @@
+//
+//  CookcademyApp.swift
+//  Cookcademy
+//
+//  Created by Andre Bezada on 27/07/22.
+//
+
+import SwiftUI
+
+@main
+struct CookcademyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
