acda56179f350bda70c7644a1997875f6d58ba2f
Andre Bezada <a.bezada.maza@gmail.com>
2022-07-27 23:58:17 -0500

    second commit

--- Cookcademy/Views/RecipesListView.swift
+++ Cookcademy/Views/RecipesListView.swift
@@ -7,15 +7,15 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct RecipesListView: View {
     var body: some View {
         Text("Hello, world!")
             .padding()
     }
 }
 
-struct ContentView_Previews: PreviewProvider {
+struct RecipesListView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        RecipesListView()
     }
 }
